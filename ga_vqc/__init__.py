eb13d21cdb171e6e7ff4a68449a662427681cb96
Thomas Sievert <63161166+tcoulvert@users.noreply.github.com>
2023-11-09 16:00:28 -0800

    added str_distance to be seen

--- ga_vqc/__init__.py
+++ ga_vqc/__init__.py
@@ -1,5 +1,5 @@
 from .GA_Manager import backend as setup
 from .Genes import Genepool
 from .simple_Individual import Individual
-from .Distance import tsne, euclidean_distances
+from .Distance import tsne, euclidean_distances, string_disctances
 from .simple_Config import Config
